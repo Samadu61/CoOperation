4c4469e0bbd11c32b3e1bf61c20470b255d058da
Samaël <villette.samael@gmail.com>
2018-03-13 12:13:19 +0100

    Authetication on Controller

--- src/CoOperation/Bundle/UserBundle/Controller/AuthenticationController.php
+++ src/CoOperation/Bundle/UserBundle/Controller/AuthenticationController.php
@@ -3,12 +3,27 @@
 namespace CoOperation\Bundle\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthenticationController extends Controller
 {
+    /**
+     * @var AuthenticationUtils
+     */
+    private $authUtils;
+
+    public function __construct(AuthenticationUtils $authUtils)
+    {
+        $this->authUtils = $authUtils;
+    }
+
     public function loginAction()
     {
-        return $this->render('@CoOperationUser/Authentication/login.html.twig');
+        $error = $this->authUtils->getLastAuthenticationError();
+
+        $lastUsername = $this->authUtils->getLastUsername();
+
+        return $this->render('@CoOperationUser/Authentication/login.html.twig', compact('error', 'lastUsername'));
     }
 
 }
