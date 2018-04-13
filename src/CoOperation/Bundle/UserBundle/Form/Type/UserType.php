4f1e8715e9b8ba9ba0e53539b724490d8b2400c8
Samaël <villette.samael@gmail.com>
2018-04-13 11:37:04 +0200

    Update security.yml & register form

--- src/CoOperation/Bundle/UserBundle/Form/Type/UserType.php
+++ src/CoOperation/Bundle/UserBundle/Form/Type/UserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,6 +24,9 @@ class UserType extends AbstractType
                 'first_options' => array('label' => 'cooperation.form.password'),
                 'second_options' => array('label' => 'cooperation.form.password_confirmation')
             ))
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-sm btn-primary']
+            ])
         ;
     }
 
