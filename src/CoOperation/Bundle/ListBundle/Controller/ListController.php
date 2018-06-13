f03d6e9cc3c08edd05028522264599cc08be1ad1
xRedSwan <Martin.gillet1996@gmail.com>
2018-06-13 17:02:23 +0200

    add list actions

--- src/CoOperation/Bundle/ListBundle/Controller/ListController.php
+++ src/CoOperation/Bundle/ListBundle/Controller/ListController.php
@@ -2,7 +2,15 @@
 
 namespace CoOperation\Bundle\ListBundle\Controller;
 
+use CoOperation\Bundle\ListBundle\Entity\UserList;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ListController extends Controller
@@ -10,5 +18,43 @@ class ListController extends Controller
     public function indexAction(): Response
     {
         return $this->render('@CoOperationList/List/index.html.twig');
+    }
+
+    public function newAction(Request $request): Response
+    {
+      $list = new UserList();
+
+    $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $list);
+
+    $formBuilder
+      ->add('name',      TextareaType::class)
+    //  ->add('products', )
+      ->add('save',      SubmitType::class)
+    ;
+
+    $form = $formBuilder->getForm();
+    if ($request->isMethod('POST')) {
+  /*    $form->handleRequest($request);
+
+      if ($form->isValid()) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($list);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Liste bien enregistrée.');
+
+        return $this->redirectToRoute('list_view', array('id' => $list->getId())); */
+      }
+    }
+
+    return $this->render('@CoOperationList/List/newlist.html.twig', array(
+      'form' => $form->createView(),
+    ));
+    }
+
+    public function viewAction($id)
+    {
+        /*$em = $this->getDoctrine()->getManager();
+        $list = $em->find()*/
     }
 }
